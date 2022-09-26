c51a04939f3f5071e7eb753108763b228b45772e
jeremyagnz <jeremyagnz@gmail.com>
2022-09-26 17:02:59 -0400

    Adding endpoint and comsuming

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -6,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+artistas: any[] = [];
+  constructor( private spotify: SpotifyService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  buscar(termino: string){
+  console.log(termino);
+  this.spotify.getArtista(termino)
+      .subscribe( (data:any) =>{
+        console.log(data.artists.items);
+      this.artistas = data.artists.items;
+      })
   }
 
 }
--- src/app/components/services/spotify.service.ts
+++ src/app/components/services/spotify.service.ts
@@ -14,5 +14,12 @@ export class SpotifyService {
     });
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
   }
-
+  
+  getArtista(termino:string){
+    const headers = new HttpHeaders({
+      Authorization:
+        'Bearer BQA_c70a5PDnY0lJP6qPMVSVB4Ze0nFe1c5-OXnHNQfBgOR19ozoh_OZhvF2k1poCinhEQpJ-Vz-fY8nNZ8w5EbYKv80qZhSr4Ki_OzHYrIlsnPrZnA',
+    });
+    return this.http.get(`https://api.spotify.com/v1/search?query=${termino}&type=artist&locale=es-ES%2Ces%3Bq%3D0.9&offset=0&limit=15`, { headers });
+  }
 }
